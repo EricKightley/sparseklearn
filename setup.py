fd0d5e72113d085e30e113145d263dc01b2b34b9
Eric Kightley <EricKightley@users.noreply.github.com>
2017-10-24 10:31:38 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='sparseklearn',
+      version='0.1',
+      url='http://github.com/EricKightley/sparseklearn',
+      author='Eric Kightley',
+      author_email='kightley.1@gmail.com',
+      license='MIT',
+      packages=['sparseklearn'],
+      zip_safe=False)
