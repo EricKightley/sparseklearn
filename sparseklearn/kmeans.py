bug fix in scaling distances

sparseklearn/kmeans.py
@@ -159,11 +159,12 @@ class KMeans(Sparsifier):
         for k in range(1,self.K):
             # distance from all the data points to the last cluster added
             latest_cluster = self.cluster_centers_[k-1,np.newaxis]
-            d_curr = self.pairwise_distances(U = latest_cluster, power = 2)
-            import pdb; pdb.set_trace()
+            d_curr = self.pairwise_distances(U = latest_cluster, power = 2)[0,:]**2
             # ||x - U|| is either this distance or the current minimum
-            d_curr = np.min((d_prev, d_curr), axis = 0)
-            # overwrite previous distances with new closest
+            # overwrite current distances where we haven't improved
+            where_we_have_not_improved = np.where(d_curr > d_prev)[0]
+            #if where_we_have_not_improved:
+            d_curr[where_we_have_not_improved] = d_prev[where_we_have_not_improved]
             d_prev = np.copy(d_curr)
 
             # compute this to normalize d_curr_sum into a prob density, and
@@ -197,7 +198,7 @@ class KMeans(Sparsifier):
 
     def _compute_labels(self):
         """ Compute the labels of each datapoint."""
-        d = self.pairwise_distances(U=self.cluster_centers_, power = 2)
+        d = self.pairwise_distances(U=self.cluster_centers_, power = 2).T
         labels_ = d.argsort(axis=1)[:,0]
         inertia_ = np.sum([d[n,labels_[n]] for n in range(self.N)])
         return [labels_, inertia_]

sparseklearn/sparsifier.py
@@ -67,7 +67,7 @@ class Sparsifier():
 
     alpha : float
         The shared compression factor. Computed as Qs/Q where Qs has been
-        chosen to make Qs/Q as close to compression_target as possible
+        chosen to make Qs/Q as close to alpha_target as possible
 
     Q : int
         Dimensionality of the mask, i.e., the number of dimensions preserved
@@ -89,9 +89,9 @@ class Sparsifier():
         """
 
         # Check type and bounds on compression_target
-        if (type(compression_target) is float and compression_target <= 0 or compression_target > 1) or \
+        if (type(compression_target) is float and (compression_target <= 0 or compression_target > 1)) or \
             (type(compression_target) is int and compression_target <= 0) or \
-            (type(compression_target) not in [int, float]):
+            (type(compression_target) not in [int, float, np.int64]):
                 raise ValueError('compression_target = {}, '.format(compression_target) + 
                     'but must be a positive integer or a float 0 < c <= 1')
 
@@ -424,7 +424,7 @@ class Sparsifier():
             # compute number of shared and random subsampling
             if type(alpha_target) is float:
                 # then alpha is the ratio of shared indices
-                Qs = int(np.floor(Q * alpha_target))
+                Qs = int(np.ceil(Q * alpha_target))
             elif type(alpha_target) is int or type(alpha_target) is np.int64:
                 # then alpha is the number of shared indices
                 Qs = alpha_target
@@ -450,11 +450,11 @@ class Sparsifier():
     def _set_RHDX(self, X, HDX, RHDX):
         """ Wrapper to compute RHDX from HDX or assign it if user-specified. """
         if RHDX is not None:
-            return RHDX
+            return RHDX.astype(float)
         elif HDX is not None:
-            return self.apply_mask(HDX)
+            return self.apply_mask(HDX.astype(float))
         else:
-            return self.apply_mask(X)
+            return self.apply_mask(X.astype(float))
 
     def fit_sparsifier(self, X = None, HDX = None, RHDX = None):
         """ Fit the sparsifier to specified data. 
@@ -544,7 +544,7 @@ class Sparsifier():
         return polynomial_combination(self.RHDX, self.mask, self.P, S, W, U, Sigma, power)
 
     def pairwise_distances(self, S = None, W = None, U = None, 
-        Sigma = None, power = 1):
+        Sigma = None, power = 2):
 
         return pairwise_distances(self.RHDX, self.mask, S, W, U, Sigma, power, self.P)
 
@@ -700,15 +700,16 @@ class Sparsifier():
     def __init__(self, compression_target = 1.0, alpha_target = 0.0, transform = 'dct',
             mask = None, precond_D = None, data_dim = None):
 
-        self._input_checker_sparsifier(compression_target, alpha_target, transform, mask, 
-                precond_D, data_dim)
-
         self.compression_target = compression_target
         self.alpha_target = alpha_target
         self.transform = transform
         self.mask = mask
         self.precond_D = precond_D
         self.data_dim = data_dim
+
+        self._input_checker_sparsifier(compression_target, alpha_target, transform, mask, 
+                precond_D, data_dim)
+
 
 
 

tests/test_kmeans.py
@@ -22,19 +22,15 @@ labels = hdf5_file['labels'][:]
 # set P
 P = 784
 
-spr = Sparsifier(mask = mask, data_dim = P)
-spr.fit_sparsifier(RHDX = RHDX)
+#spr = Sparsifier(mask = mask, data_dim = P)
+#spr.fit_sparsifier(X = X)
 
-test0 = spr.pairwise_distances(U = RHDX[5:10], S = np.array([4,9,12]))
-test1 = spr.pairwise_distances(S = np.array([4,9,12]))
-
-#skm = KMeans(n_clusters = 3)
+#skm = KMeans(n_clusters = 3, compression_target = 0.1)
 #skm.fit(X=X)
 
-#skm = KMeans(mask = mask, data_dim = P, n_clusters = 3)
-#skm = KMeans(n_clusters = 3, compression_target = 0.03, precond_D = precond_D)
-#skm.fit(HDX=HDX)
-#localpath = '/home/eric/Dropbox/EricStephenShare/sparseklearn/plots/'
-
-#np.save(localpath+'kmeans_means.npy', skm.cluster_centers_)
-#np.save(localpath+'kmeans_counts.npy', skm.n_per_cluster)
+skm = KMeans(mask = mask, data_dim = P, precond_D = precond_D, n_clusters = 3)
+#skm = KMeans(n_clusters = 3, precond_D = precond_D)
+skm.fit(RHDX=RHDX)
+localpath = '/home/eric/Dropbox/EricStephenShare/sparseklearn/plots/'
+np.save(localpath+'kmeans_means.npy', skm.cluster_centers_)
+np.save(localpath+'kmeans_counts.npy', skm.n_per_cluster)

tests/test_sparsifier.py
@@ -5,6 +5,8 @@ from sparseklearn import Sparsifier
 from sparseklearn import KMeans
 from auxutils import generate_mnist_dataset
 
+from sklearn.random_projection import GaussianRandomProjection as grp
+from sklearn.random_projection import johnson_lindenstrauss_min_dim as jlmindim
 
 # set the random seed
 rs = 22
@@ -13,15 +15,34 @@ np.random.seed(rs)
 # load computed data
 hdf5_file = h5py.File('/home/eric/kmeansdata/sample_mnist.hdf5','r')
 X = hdf5_file['X'][:]
-HDX = hdf5_file['HDX'][:]
-RHDX = hdf5_file['RHDX'][:]
-mask = hdf5_file['mask'][:]
-precond_D = hdf5_file['precond_D'][:]
-labels = hdf5_file['labels'][:]
-# set P
+#HDX = hdf5_file['HDX'][:]
+#RHDX = hdf5_file['RHDX'][:]
+#mask = hdf5_file['mask'][:]
+#precond_D = hdf5_file['precond_D'][:]
+#labels = hdf5_file['labels'][:]
+
+X = X[:1000]
+N = X.shape[0]
 P = 784
 
-spr = Sparsifier(mask = mask, data_dim = P)
-spr.fit_sparsifier(RHDX = RHDX)
+#Q = jlmindim(n_samples = N, eps = 0.5)
+Q = 30
+
+compression_target = float(Q/P)
+
+spr = Sparsifier(compression_target = compression_target, alpha_target = 0.0, transform = 'dct')
+spr.fit_sparsifier(X = X)
+pd = spr.pairwise_distances()
+
+
+#from sklearn.metrics.pairwise import pairwise_distances
+#pd_true = pairwise_distances(X)
+#pd_grp = pairwise_distances(grp(n_components = Q).fit_transform(X))
+#I = np.identity(N)
+#normalizer = pd_true + np.identity(N)
+#pd = (pd + I) / normalizer
+#pd_grp = (pd_grp + I) / normalizer
+
+
 
 